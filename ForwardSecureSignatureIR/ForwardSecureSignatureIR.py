29a3a7b01aadbc0b110114229efedf54c5301666
max <ttyhgo@gmail.com>
2016-02-11 00:19:08 +0900

    Forward Secure Signature IR scheme without check e

---
+++ ForwardSecureSignatureIR/ForwardSecureSignatureIR.py
@@ -0,0 +1,152 @@
+from Crypto.PublicKey.pubkey import *
+from Crypto.Util import number
+from Crypto import Random
+from Crypto.Hash import SHA256
+from Crypto.PublicKey.pubkey import *
+import time
+
+class FSSIR():
+    def __init__(self, k, l, T, randfunc=None):
+        if randfunc is None:
+            self._randfunc = Random.new().read
+        self._k = k
+        self._l = l
+        self._T = T
+
+    def gensafeprime(self):
+        while True:
+            q = number.getPrime(self._k/2-1, self._randfunc)
+            p = 2*q+1
+            if number.isPrime(p, false_positive_prob=1e-06, randfunc=self._randfunc):
+                return p
+
+    def randomingroupstar(self, N):
+        while True:
+            r = number.getRandomRange(3, N, self._randfunc)
+            if 1 == GCD(r, N):
+                return r
+
+    def keygen(self):
+        p1 = self.gensafeprime()
+        p2 = self.gensafeprime()
+
+        n = p1*p2
+        phin = (p1-1)*(p2-1)
+        t1 = self.randomingroupstar(n)
+        e=[]
+        for i in range(0, self._T):
+            e.append(number.getPrime(self._l, self._randfunc))
+
+        f2 = 1
+        for i in range(1, self._T):
+            f2 = f2*e[i] % phin
+
+        s1 = pow(t1, f2, n)
+        v = inverse(pow(s1, e[0], n), n)
+        t2 = pow(t1, e[0], n)
+
+        i = 0
+        sk = [i, self._T, n, s1, t2, e[i], self._randfunc]
+        pk = [n, v, self._T]
+
+        return sk, pk
+
+    def update(self, sk):
+        j = sk[0]
+        T = sk[1]
+        n = sk[2]
+        sj = sk[3]
+        tj = sk[4]
+        e = sk[5]
+        randfunc = sk[6]
+
+        if j == T-1:
+            return None
+        newe=[]
+        for i in range(0, self._T):
+            if i <= j:
+                newe.append(0)
+            else:
+                newe.append(getPrime(self._l, randfunc))
+        sj = 1
+        for i in range(j+1, T):
+            sj = sj*pow(tj, newe[i], n) % n
+        tj = pow(tj, newe[j+1], n)
+        return (j+1, T, n, sj, tj, newe[j+1], randfunc)
+
+    def sign(self, sk, M):
+        j = sk[0]
+        T = sk[1]
+        n = sk[2]
+        sj = sk[3]
+        tj = sk[4]
+        e = sk[5]
+        randfunc = sk[6]
+
+        r = self.randomingroupstar(n)
+        y = pow(r, e, n)
+        h = SHA256.new()
+        h.update(long_to_bytes(j))
+        h.update(long_to_bytes(e))
+        h.update(long_to_bytes(y))
+        h.update(M)
+        sig = bytes_to_long(h.digest())
+        z = r*pow(sj, sig, n) % n
+        return [z, sig, j, e]
+
+    def verify(self, pk, M, signature):
+        n = pk[0]
+        v = pk[1]
+        T = pk[2]
+
+        z = signature[0]
+        sig = signature[1]
+        j = signature[2]
+        e = signature[3]
+
+        #if e >= pow(2, self._l)*(1+(j+1)/T) or e < pow(2, self._l):
+        #    return False
+        if z % n == 0:
+            return False
+        y_ver = pow(z, e, n) * pow(v, sig, n) % n
+        h = SHA256.new()
+        h.update(long_to_bytes(j))
+        h.update(long_to_bytes(e))
+        h.update(long_to_bytes(y_ver))
+        h.update(M)
+        if sig == bytes_to_long(h.digest()):
+            return True
+        else:
+            return False
+
+
+'''Test Vector'''
+fssir = FSSIR(2048, 160, 1000)
+
+start = time.time()
+sk, pk = fssir.keygen()
+print "Keygen : %.8f" %(time.time() - start)
+
+start = time.time()
+signature = fssir.sign(sk, "hello")
+print "Sign : %.8f" %(time.time() - start)
+
+start = time.time()
+print fssir.verify(pk, "hello", signature)
+print "Verify : %.8f" %(time.time() - start)
+
+start = time.time()
+fssir.update(sk)
+print "Update : %.8f" %(time.time() - start)
+
+start = time.time()
+signature2 = fssir.sign(sk, "hello2")
+print "Sign : %.8f" %(time.time() - start)
+
+start = time.time()
+print fssir.verify(pk, "hello", signature)
+print "Verify : %.8f" %(time.time() - start)
+
+start = time.time()
+print fssir.verify(pk, "hello2", signature2)
+print "Verify : %.8f" %(time.time() - start)
